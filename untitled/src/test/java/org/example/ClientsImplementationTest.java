8bb2a7a99e58cf810ac78ba7384b74ce4ecc5e86
WZ-TEB <wiktor.zareba@teb.edu.pl>
2024-09-24 08:45:22 +0200

    added multiple ClientsImplementationTest methods

--- untitled/src/test/java/org/example/ClientsImplementationTest.java
+++ untitled/src/test/java/org/example/ClientsImplementationTest.java
@@ -3,6 +3,7 @@ package org.example;
 import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class ClientsImplementationTest {
     private static final ClientsImplementation testClientsImplementation = new ClientsImplementation();
@@ -18,18 +19,24 @@ class ClientsImplementationTest {
     }
 
     @Test
-    void getNumberOfClients() {
-    }
-
-    @Test
     void getClientName() {
+        //given
+        ArrayList<Client> testArrayList = new ArrayList<>();
+        testClientsImplementation.createNewClient("testFirstName","testLastName");
+        //when
+        String testClientFullName = testClientsImplementation.getClientName(0);
+        //then
+        assertEquals("testFirstName testLastName",testClientFullName);
     }
 
     @Test
     void activatePremiumAccount() {
-    }
-
-    @Test
-    void isClientPremium() {
+        //given
+        ArrayList<Client> testArrayList = new ArrayList<>();
+        testClientsImplementation.createNewClient("testFirstName","testLastName");
+        //when
+        testClientsImplementation.activatePremiumAccount(0);
+        //then
+        assertTrue(testClientsImplementation.isClientPremium(0));
     }
 }
